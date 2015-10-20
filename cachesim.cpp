37513e1c533eba0eea4ebaed4ce91aecc829c519
Sunimal Rathnayake <sunimalr@gmail.com>
2015-10-20 16:54:29 +0800

    reading success + bit masking

--- cachesim.cpp
+++ cachesim.cpp
@@ -1,34 +1,45 @@
 #include <cstdio>
 #include <iostream>
+#include <cstdlib>
+#include <bitset>
+
 using namespace std;
 
 int main()
 {
-	FILE *file;
-	unsigned long long *buffer;
-	long size;
-	size_t readstatus;
-
-	file=fopen("ls.trace","rb");
-	if(file==NULL)
-	{
-		cout<<"File Open Error"<<endl;
-		//exit(1);
-	}
-
-	fseek(file, 0, SEEK_END);
-    size=ftell(file);
-    rewind(file);
-    int itr;
-    for(itr=1;itr<5;itr++)
-    {
-    	readstatus=fread(buffer,8,1,file);
-    	cout<<"read :"<<buffer<<endl;	
-    }
+	FILE *rm;
+    unsigned long long buf[1];
+    char b[9];
+    int i;
+    long fSize;
     
-    fclose(file);
+    rm = fopen("ls.trace", "rb");
+    //rm = fopen("test.txt", "r");
+    buf[0]=0;
 
+    // obtain file size:
+  	fseek (rm , 0 , SEEK_END);
+  	fSize = ftell (rm);
+  	rewind (rm);
+  	//cout<<fSize<<endl;
+  	long ctr=fSize/8;
+    if (rm != NULL) {
+    	for(i=0;i<ctr;i++)
+    	{
+        fread(buf,8 , 1, rm);
+        //cout<<bitset<64>(buf[0])<<endl
+        //cout<<hex<<buf[0]<<endl;
+        //cout<<(buf[0]&0x8000000000000000)<<endl;
+        //cout<<(buf[0]&0x4000000000000000)<<endl;
+	    }
+	    fclose(rm);
+	    cout<<"END"<<endl;
+    }
+    else
+        printf("File Not Found.\r\n");
+	
 
 	return 0;
 }
+
 
