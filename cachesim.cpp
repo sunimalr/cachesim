95c938ac1dbefded840546bba06a2466e4bf5882
Sunimal Rathnayake <sunimalr@gmail.com>
2015-10-20 20:22:02 +0800

    structured into functions

--- cachesim.cpp
+++ cachesim.cpp
@@ -3,7 +3,13 @@
 #include <cstdlib>
 #include <bitset>
 
+#define FETCH 100
+#define LOAD 200
+#define STORE 300
+
 using namespace std;
+int process(unsigned long long address, string instruction);
+int detect_instr_type(unsigned long long buffer);
 
 int main()
 {
@@ -26,11 +32,23 @@ int main()
     if (rm != NULL) {
     	for(i=0;i<ctr;i++)
     	{
-        fread(buf,8 , 1, rm);
-        //cout<<bitset<64>(buf[0])<<endl
-        //cout<<hex<<buf[0]<<endl;
-        //cout<<(buf[0]&0x8000000000000000)<<endl;
-        //cout<<(buf[0]&0x4000000000000000)<<endl;
+        	fread(buf,8 , 1, rm);
+        	//cout<<bitset<64>(buf[0])<<endl
+        	//cout<<hex<<buf[0]<<endl;
+        	//cout<<(buf[0]&0x8000000000000000)<<endl;
+        	//cout<<(buf[0]&0x4000000000000000)<<endl;
+        		switch(detect_instr_type(buf[0]))
+        		{
+        			case FETCH :
+        				process(buf[0],FETCH);
+        				break;
+        			case LOAD :
+        				process(buf[0],LOAD);
+        				break :
+        			case STORE :
+        				process(buf[0],STORE);
+        				break;
+        		}
 	    }
 	    fclose(rm);
 	    cout<<"END"<<endl;
@@ -42,4 +60,13 @@ int main()
 	return 0;
 }
 
+int process(unsigned long long address, int instruction)
+{
+	if(address)
+	return 0;
+}
 
+int detect_instr_type(unsigned long long buffer)
+{
+	return 0;
+}
