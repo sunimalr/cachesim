9ebc9454028053bc79ce4623dbc2a783ac10281e
Sunimal Rathnayake <sunimalr@gmail.com>
2015-10-21 14:26:26 +0800

    L1i cache structure

--- cache.h
+++ cache.h
@@ -1,10 +1,17 @@
-class Cache
-{
-	string type;
-	long size;
-};
+#ifndef CACHE_H
+#define CACHE_H
 
-class L1iCache : public Cache
-{
-	long long [][]
-}
+struct cache_line_block{
+	int valid;
+	unsigned long long tag;
+};
+typedef struct cache_line_block cache_line_block;
+
+struct l1i_cache_set{
+	cache_line_block block[8];
+};
+typedef l1i_cache_set l1i_cache_set;
+
+l1i_cache_set cache_l1i[64];
+
+int l1i_fetch(unsigned long long address);
---
+++ cache_l1i.cpp
@@ -0,0 +1,2 @@
+//cache.cpp
+#include <iostream>
